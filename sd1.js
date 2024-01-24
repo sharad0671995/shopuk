01807f0a07168ee2b21783e9be2f252ee1cadbde
sharad0671995 <96901154+sharad0671995@users.noreply.github.com>
2024-01-24 20:56:20 +0530

    DoneFinal

---
+++ sd1.js
@@ -0,0 +1,14 @@
+const bar = document.getElementById("bar");
+const navbar = document.getElementById("navbar");
+const close = document.getElementById("close");
+if (bar) {
+  bar.addEventListener("click", () => {
+    navbar.classList.add("active");
+  });
+}
+
+if (close) {
+  bar.addEventListener("click", () => {
+    navbar.classList.remove("active");
+  });
+}
